0764b84601d0b22b1c8509b40d2dfd4e6c339fef
tylerfyu <tylerfyu@outlook.com>
2022-02-09 08:50:45 -0800

    :sparkles: Feat: Implement the feature to get information of coins

--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [coinlist, setCoinList] = useState([]);
+  const [selectedCoin, setSelectedCoin] = useState(null);
+
+  useEffect(() => {
+    fetch('https://api.coincap.io/v2/assets?limit=50&offset=0')
+    .then((res) => res.json())
+    .then((data) => {
+      setCoinList(data.data);
+      const bitcoin_coin = data.data.filter(cell => cell.id == 'bitcoin-bep2').at(0);
+      setSelectedCoin(bitcoin_coin);
+    })
+    .catch((err)=>console.log(err))
+  }, []);
+
+  const handleChangeCoin = (event) => {
+    getInformation(event.target.value);
+  }
+
+  const getInformation = id => {
+    const selected_coin = coinlist.filter(cell => cell.id == id).at(0);
+    setSelectedCoin(selected_coin);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <span>Select the Coin Type: </span>
+        <select className="coin-select" onChange={handleChangeCoin} value={coinlist.length == 0 ? '' : 'bitcoin-bep2'}> 
+          {
+            coinlist.map(cell => {
+              return(<option key={cell.id} value={cell.id}>{cell.name} - {cell.symbol}</option>)
+            })
+          }
+        </select>
+      </div>   
+      <div>
+        <span>Supply: </span>
+        <span>{selectedCoin? selectedCoin.supply : ''}</span>
+      </div> 
+      <div>
+        <span>MarketCap: </span>
+        <span>{selectedCoin? selectedCoin.marketCapUsd : ''}</span>
+      </div>
     </div>
   );
 }
